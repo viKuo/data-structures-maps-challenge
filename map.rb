dff332355cf641e036899070669a69fa79bc4102
viKuo <lost.angel.fin@gmail.com>
2016-08-12 19:00:27 -0500

    finish implementing map

--- map.rb
+++ map.rb
@@ -1,3 +1,35 @@
 class Map
+	def initialize
+		@keys = []
+		@values = []
+	end
+
+	def set(key, value)
+		@keys << key
+		@value << values
+	end
+
+	def get(key)
+		index = [@keys.index(key)
+		KeyError if index == nil
+		@value[index]
+	end
+
+	def has_key?(key)
+		@key.include?(key)
+	end
+
+	def remove(key)
+		index = [@keys.index(key)
+		KeyError if index == nil
+		@keys.delete_at(index)
+		@values.delete_at(index)
+	end
+
+	def iterate
+		for i in 0..@keys.length
+			yield(@values[i], @keys[i])
+		end
+	end
 
 end
